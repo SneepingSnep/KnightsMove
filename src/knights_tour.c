05b16927593e49a8e2812d8d68ffaf224c23381e
Sebastian <Seb@noerlev.dk>
2025-10-19 00:24:07 +0200

    getting the board to update with a move.

--- src/knights_tour.c
+++ src/knights_tour.c
@@ -1,1 +1,31 @@
 #include "../headers/knights_tour.h"
+#include <stddef.h>
+#define MOVE_COUNT 8 /**< Number of moves that a knight can make */
+const int MOVES_X[MOVE_COUNT] = {2, 1, -1, -2, -2, -1, 1, 2};
+const int MOVES_Y[MOVE_COUNT] = {1, 2, 2, 1, -1, -2, -2, -1};
+bool move_is_possible(size_t move_id, size_t *x, size_t *y, board_t visited) {
+  // Check om det er et lovligt move(Lovligt hvis vi ikke har været der, og vi
+  // ikke ryger ud af brættet hvis vi gør det) hvis det er lovligt så ryk
+  // derhend, og returner true (evt lav move her også) hvis det ikke er lovlig
+  // returner false.
+  int tempX = *x + MOVES_X[move_id];
+  int tempY = *y + MOVES_Y[move_id];
+
+  if ((int *)x + (int)MOVES_X[move_id] >= 0 &&
+      (int *)y + (int)MOVES_Y[move_id] >= 0) {
+    visited[tempX][tempY] = 1;
+    return true;
+  }
+
+  return false;
+}
+
+void printBoard(board_t board) {
+  for (size_t i = 0; i < SIZE; i++) {
+    for (size_t j = 0; j < SIZE; j++) {
+
+      printf("%-2llu ", board[i][j]);
+    }
+    puts("");
+  }
+}
--- src/main.c
+++ src/main.c
@@ -7,40 +7,63 @@
 int input = 0;
 int chssqrinterval[] = {8, 16, 32, 40, 48, 56, 64};
 int loopstatus = 1;
-int firstcordinate, secondcordinate;
+size_t *firstcordinate, *secondcordinate;
+size_t StartX, StartY;
 board_t chess = {};
 bool fuck = true;
 int main() {
   srand(time(NULL));
-  while (fuck) {
-    size_t rando = rand() % 63;
+  StartX = 4;
+  StartY = 6;
+  firstcordinate = &StartX;
+  secondcordinate = &StartY;
+  chess[*firstcordinate][*secondcordinate] = 1;
+  // size_t count = 0;
+  // printBoard(chess);
+  // for (size_t i = 0; i < SIZE; i++) {
+  //   for (size_t j = 0; j < SIZE; j++) {
+  //     chess[i][j] = count;
+  //     count++;
+  //   }
+  // }
 
-    firstcordinate = (rando / 8);
-    secondcordinate = (rando % 8);
-    printf("%d ", firstcordinate);
-    printf("%d\n", secondcordinate);
-    chess[firstcordinate][secondcordinate] = 1;
-    printf("rando numbo: %llu\n", rando);
+  puts("");
+  printBoard(chess);
+  while (fuck) {
+
+    if (move_is_possible(4, firstcordinate, secondcordinate, chess)) {
+    }
+
+    // size_t rando = rand() % 63;
+
+    // firstcordinate = (rando / 8);
+    // secondcordinate = (rando % 8);
+    // printf("%d ", firstcordinate);
+    // printf("%d\n", secondcordinate);
+    // chess[firstcordinate][secondcordinate] = 1;
+    // printf("rando numbo: %llu\n", rando);
+    // // for (size_t i = 0; i < SIZE; i++) {
+    // //   for (size_t j = 0; j < SIZE; j++) {
+    // //     if (((i + 1) * (j + 1)) == rando) {
+    // //       chess[i][j] = 1;
+    // //       loopstatus = 0;
+    // //       break;
+    // //     }
+    // //   }
+    // //   if (loopstatus == 0)
+    // //     break;
+    // //   puts("");
+    // // }
     // for (size_t i = 0; i < SIZE; i++) {
     //   for (size_t j = 0; j < SIZE; j++) {
-    //     if (((i + 1) * (j + 1)) == rando) {
-    //       chess[i][j] = 1;
-    //       loopstatus = 0;
-    //       break;
-    //     }
+
+    //     printf("%llu ", chess[i][j]);
     //   }
-    //   if (loopstatus == 0)
-    //     break;
     //   puts("");
     // }
-    for (size_t i = 0; i < SIZE; i++) {
-      for (size_t j = 0; j < SIZE; j++) {
-
-        printf("%llu ", chess[i][j]);
-      }
-      puts("");
-    }
-    printf("Want to try again?");
+    // printf("Want to try again?");
+    puts("");
+    printBoard(chess);
     scanf("%d", &input);
     if (input == 1) {
       break;
